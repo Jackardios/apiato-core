79ccb9c850c1e23b68d710c4df7d57d80cf00161
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-08-05 04:21:01 +0300

    fix resource api controller

--- Abstracts/Resources/JsonResource.php
+++ Abstracts/Resources/JsonResource.php
@@ -2,9 +2,14 @@
 
 namespace Apiato\Core\Abstracts\Resources;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\JsonResource as LaravelJsonResource;
 
 abstract class JsonResource extends LaravelJsonResource
 {
-
+    public function created(int $status = 201): JsonResponse
+    {
+        return $this->toResponse()
+                    ->setStatusCode($status);
+    }
 }
