6dc0aa05e97ed7daae4346f16248963484f30f49
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-08-05 18:26:12 +0300

    fix api resource abstracts and stubs

--- Abstracts/Resources/Json/JsonResource.php
+++ Abstracts/Resources/Json/JsonResource.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Apiato\Core\Abstracts\Resources;
+namespace Apiato\Core\Abstracts\Resources\Json;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\JsonResource as LaravelJsonResource;
---
+++ Abstracts/Resources/Json/ResourceCollection.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Apiato\Core\Abstracts\Resources\Json;
+
+use Illuminate\Http\Resources\Json\ResourceCollection as LaravelResourceCollection;
+
+abstract class ResourceCollection extends LaravelResourceCollection
+{
+}
