996dc9def5a60b5f9e311437909adb0757709d48
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2021-08-11 15:51:02 +0300

    refactor ResponseTrait

--- Traits/ResponseTrait.php
+++ Traits/ResponseTrait.php
@@ -3,50 +3,31 @@
 namespace Apiato\Core\Traits;
 
 use Illuminate\Http\JsonResponse;
-use ReflectionClass;
 
 trait ResponseTrait
 {
-    protected array $metaData = [];
-
-    public function withMeta($data): self
-    {
-        $this->metaData = $data;
-
-        return $this;
-    }
-
-    public function json($message, $status = 200, array $headers = [], $options = 0): JsonResponse
+    public function json($message, int $status = 200, array $headers = [], int $options = 0): JsonResponse
     {
         return new JsonResponse($message, $status, $headers, $options);
     }
 
-    public function created($message = null, $status = 201, array $headers = [], $options = 0): JsonResponse
+    public function created($message = null, int $status = 201, array $headers = [], int $options = 0): JsonResponse
     {
         return new JsonResponse($message, $status, $headers, $options);
     }
 
-    public function deleted($responseArray = null): JsonResponse
-    {
-        if (!$responseArray) {
-            return $this->accepted();
-        }
-
-        $id = $responseArray->getKey();
-        $className = (new ReflectionClass($responseArray))->getShortName();
-
-        return $this->accepted([
-            'message' => "$className ($id) Deleted Successfully.",
-        ]);
-    }
-
-    public function accepted($message = null, $status = 202, array $headers = [], $options = 0): JsonResponse
+    public function deleted($message = null, int $status = 204, array $headers = [], int $options = 0): JsonResponse
     {
         return new JsonResponse($message, $status, $headers, $options);
     }
 
-    public function noContent($status = 204): JsonResponse
+    public function accepted($message = null, int $status = 202, array $headers = [], int $options = 0): JsonResponse
     {
-        return new JsonResponse(null, $status);
+        return new JsonResponse($message, $status, $headers, $options);
+    }
+
+    public function noContent(int $status = 204, array $headers = [], int $options = 0): JsonResponse
+    {
+        return new JsonResponse(null, $status, $headers, $options);
     }
 }
